e2eff40b1b3ecdda99adaa658afdb22191bc712c
jay183145 <jay183145@gmail.com>
2022-05-12 09:53:40 +0800

    404 page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views//jobs/Jobs.vue'
 import JobDetails from '../views//jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -26,6 +27,17 @@ const routes = [
     component: JobDetails,
     props: true
   },
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // 404 catchall
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
+  }
 ]
 
 const router = createRouter({
